03065fc11e955081dbf7ad3f5e8957e3f77722f0
OM Gupta <om.siplservices.com>
2022-09-30 01:09:45 +0530

    1. User Controller added.

---
+++ DemoCoreApi/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Library.Model;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoCoreApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		// GET: api/<UserController>
+		[HttpPost]
+		[Route("Search")]
+		[SwaggerOperation(Summary = "Search user", Description = "My description")]
+		public ActionResult<string[]> Search([FromBody] SearchUserDto search)
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<UserController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<UserController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<UserController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<UserController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ Library.Model/User/SearchUserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+	public class SearchUserDto
+	{
+		[Required(AllowEmptyStrings = false,ErrorMessage = "Name is required")]
+		public string Name { get; set; } = string.Empty;
+		public int Age{ get; set; } = 0;
+		public string Gender { get; set; } = string.Empty;
+
+	}
+}
